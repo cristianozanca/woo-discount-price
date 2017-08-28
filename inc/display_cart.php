73fe2ce8891bcce7d1b62b2a7bb169b9dd5d65a1
rememb48 <corrado@debian>
2017-08-28 17:52:24 +0200

    ritorno a vecchia funzione woodiscpr_change_cart_table_price_display

--- inc/display_cart.php
+++ inc/display_cart.php
@@ -3,8 +3,21 @@
 // show discount price in cart page
 
 function woodiscpr_change_cart_table_price_display( $price, $values, $cart_item_key ) {
-	$prezzo_scontato = wc_format_decimal($values['data']->get_sale_price(),2 );
-	$prezzo_pieno = wc_format_decimal($values['data']->get_regular_price(),2);
+	$slashed_price = $values['data']->get_price_html();
+	$is_on_sale = $values['data']->is_on_sale();
+	if ( $is_on_sale ) {
+		$price = $slashed_price;
+	}
+	return $price;
+}
+
+##################################
+/*
+ * alternative code , decimal separator point instead of comma
+ *
+function woodiscpr_change_cart_table_price_display( $price, $values, $cart_item_key ) {
+	$prezzo_scontato = wc_format_decimal($values['data']->get_sale_price(),2, ''  );
+	$prezzo_pieno = wc_format_decimal($values['data']->get_regular_price(),2, '','');
 	$valuta = get_woocommerce_currency_symbol();
 	$is_on_sale = $values['data']->is_on_sale();
 	if ( $is_on_sale ) {
@@ -12,3 +25,6 @@ function woodiscpr_change_cart_table_price_display( $price, $values, $cart_item_
 	}
 	return $price;
 }
+
+*/
+###############################
